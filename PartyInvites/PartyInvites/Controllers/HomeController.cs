f779d48251d49402209da0d1423a12147f7521f5
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2022-03-25 19:12:16 +0300

    add PartyInvites project

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Diagnostics;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+        
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                GuestRepository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }   
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(GuestRepository.Responses.Where(x => x.WillAttend == true));
+        }
+
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/GuestRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace PartyInvites.Models
+{
+    public static class GuestRepository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+        public static void AddResponse(GuestResponse guestResponse)
+        {
+            responses.Add(guestResponse);
+        }
+    }
+}
