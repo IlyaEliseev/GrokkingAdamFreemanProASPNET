034d75b491b39bb649a11a0c80ed3510363d2817
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2022-03-27 16:23:03 +0300

    edit Razor project

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Price = 275M },
+                new Product {Name = "LifeJacket", Price = 48.95M },
+                new Product {Name = "SoccerBall", Price = 19.50M },
+                new Product {Name = "CornerFlag", Price = 34.95M },
+            };
+
+            return View(products);
+        }
+    }
+}
