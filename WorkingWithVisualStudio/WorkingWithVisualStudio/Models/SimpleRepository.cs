1d54983d97492975f1ab3a108b66239d0a4a420e
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2022-03-29 17:19:41 +0300

    add WorkWithVisualStudio project

---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio/Models/SimpleRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace WorkingWithVisualStudio.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository simpleRepository 
+            = new SimpleRepository();
+
+        private Dictionary<string, Product> _products 
+            = new Dictionary<string, Product>();
+
+        public static SimpleRepository ShareRepository => simpleRepository;
+
+        public SimpleRepository()
+        {
+            var products = new[] {
+                new Product {Name = "Kayak", Price = 275M },
+                new Product {Name = "LifeJacket", Price = 48.95M },
+                new Product {Name = "SoccerBall", Price = 19.50M },
+                new Product {Name = "CornerFlag", Price = 34.95M },
+            };
+
+            foreach (var product in products)
+            {
+                AddProduct(product);
+            }
+        }
+
+        public IEnumerable<Product> Products => _products.Values;
+
+        public void AddProduct(Product product) => _products.Add(product.Name, product);
+    }
+}
